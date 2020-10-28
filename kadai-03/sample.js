c7eb107a525a6f521f3e67af767930dd0b736025
Kamata Ryo <mugil.cephalus+github@gmail.com>
2020-10-28 04:57:37 +0000

    課題3を追加

---
+++ kadai-03/sample.js
@@ -0,0 +1,21 @@
+const score = 50
+
+if (false) {
+    console.log('S')
+} else if (score >= 80) {
+    console.log('A')
+} else if (score >= 70) {
+    console.log('B')
+} else if (score >= 60) {
+    console.log('C')
+} else {
+    console.log('F')
+}
+
+// for (let n = 1; n < 10000; n = n + 1) {
+//     if (n % 3 === 1) {
+//         console.log(n)
+//     }
+// }
+
+
